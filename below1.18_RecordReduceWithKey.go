03639cd5955902c9f8c3587cad15e1f39be05de1
Johnny Lin <57846020+JohnnyLin-a@users.noreply.github.com>
2021-12-13 12:01:44 -0500

    Add below 1.18 solution

---
+++ below1.18_RecordReduceWithKey.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"errors"
+	"log"
+	"os"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+func reflectKeyExists(v *reflect.Value, keyValue *reflect.Value) bool {
+	// Check if key exists
+	return v.MapIndex(*keyValue).IsValid()
+}
+
+/*
+	RecordReduceWithKey sorts records and reduces them by keys specified in the indexing param.
+
+	The pointer passed in outputRecord is directly modified (Similar to json.Unmarshal).
+*/
+func RecordReduceWithKey(records interface{}, outputRecord interface{}, indexing ...string) error {
+	if reflect.TypeOf(outputRecord).Kind() != reflect.Ptr || reflect.Indirect(reflect.ValueOf(outputRecord)).Kind() != reflect.Map {
+		return errors.New("outputRecord must be a pointer to a map")
+	}
+
+	if reflect.Indirect(reflect.ValueOf(outputRecord)).IsNil() {
+		return errors.New("outputRecord cannot be nil pointer")
+	}
+	output := reflect.Indirect(reflect.ValueOf(outputRecord))
+
+	recordsType := reflect.TypeOf(records)
+
+	if recordsType.Kind() != reflect.Slice {
+		return errors.New("records is not a slice, got: " + recordsType.Kind().String())
+	}
+	recordsVal := reflect.ValueOf(records)
+
+	for i := 0; i < recordsVal.Len(); i++ {
+		record := recordsVal.Index(i)
+		currentLevel := output
+		var tPropValue reflect.Value
+
+		// Traverse into indexing
+		for i, prop := range indexing {
+			// Check if prop is exported
+			if prop[0:1] != strings.ToUpper(prop[0:1]) {
+				return errors.New("prop must be pubic: " + prop)
+			}
+
+			// Validate T's prop type
+			tPropValue = record.FieldByName(prop)
+			if !tPropValue.IsValid() {
+				return errors.New("invalid prop name of " + prop)
+			}
+			tPropKind := record.FieldByName(prop).Kind()
+			switch tPropKind {
+			case reflect.Int, reflect.String:
+				break
+			default:
+				return errors.New("invalid prop type, must be string, int, but got " + tPropKind.String())
+			}
+
+			// Validate map type
+			if currentLevel.Kind() != reflect.Map {
+				return errors.New("output is not a map")
+			}
+
+			// Validate map key type and T type
+			if currentLevel.Type().Key().Kind() != tPropKind {
+				return errors.New("Key datatype does not match for " + currentLevel.Type().String() + ", got " + tPropKind.String())
+			}
+
+			// Check if key exists
+			found := reflectKeyExists(&currentLevel, &tPropValue)
+
+			// Create key if key does not exist
+			if !found && len(indexing) != i+1 {
+				if currentLevel.Type().Elem().Kind() == reflect.Map {
+					nextMapType := currentLevel.Type().Elem()
+					currentLevel.SetMapIndex(tPropValue, reflect.MakeMap(nextMapType))
+					found = true
+				} else {
+					return errors.New("cannot create new map for next level. There is/are " + strconv.Itoa(len(indexing)) + " indexing key(s), therefore should have the same amount of map(s)")
+				}
+			}
+
+			// Check if final level is slice or not and add record
+			if len(indexing) == i+1 {
+				// Validate key kind
+				if currentLevel.Type().Key().Kind() != tPropKind {
+					return errors.New("map key datatype incorrect. map key type of " + currentLevel.Type().Key().Kind().String() + " but got " + tPropKind.String() + " prop")
+				}
+
+				if currentLevel.Type().Elem().Kind() == reflect.Slice {
+					if !found {
+						nextSliceType := currentLevel.Type().Elem()
+						if record.Type() != nextSliceType.Elem() {
+							return errors.New("cannot assign slice's value of datatype " + nextSliceType.Elem().String() + " for record of type " + record.Type().String())
+						}
+						currentLevel.SetMapIndex(tPropValue, reflect.Append(reflect.MakeSlice(nextSliceType, 0, 0), record))
+					} else {
+						currentLevel.SetMapIndex(tPropValue, reflect.Append(currentLevel.MapIndex(tPropValue), record))
+					}
+				} else {
+					if record.Type() != currentLevel.Type().Elem() {
+						return errors.New("cannot assign map's value of datatype " + currentLevel.Type().Elem().String() + " for record of type " + record.Type().String())
+					}
+					currentLevel.SetMapIndex(tPropValue, record)
+				}
+			}
+
+			// Update currentLevel
+			if found {
+				currentLevel = currentLevel.MapIndex(tPropValue)
+			}
+		}
+	}
+	return nil
+}
+
+type record1 struct {
+	Record1_id int
+	Val1       string
+}
+
+type recordx struct{}
+
+func main() {
+	records := []record1{{1, "val"}, {1, "val"}, {3, "val"}, {4, "val2"}, {3, "val2"}}
+
+	output := make(map[string]map[int][]record1)
+
+	err := RecordReduceWithKey(records, &output, "Val1", "Record1_id")
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	log.Println(output)
+	/*
+		expected output:
+		output
+			["val"]
+				[1]
+					[{1, "val"}, {1, "val"}]
+				[3]
+					[{3, "val"}]
+			["val2"]
+				[3]
+					[{3, "val2"}]
+				[4]
+					[{4, "val2"}]
+	*/
+}
